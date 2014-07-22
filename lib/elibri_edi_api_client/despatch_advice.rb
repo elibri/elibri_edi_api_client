2bda9f518f2baffdb455c7a2c630d0e92b8ae864
Piotr Romańczuk <porbas@gmail.com>
2014-07-22 19:20:37 +0200

    DespatchAdvice.create method

--- lib/elibri_edi_api_client/despatch_advice.rb
+++ lib/elibri_edi_api_client/despatch_advice.rb
@@ -10,5 +10,14 @@ module ElibriEdiApiClient
       o
     end
 
+    def self.create(data)
+      unless data[:edi_purchase_order_id]
+        fail InputDataError.new "Can't create DespatchAdvice without :edi_purchase_order_id provided"
+      else
+        po = PurchaseOrder.new data[:edi_purchase_order_id]
+        po.create_message data
+      end
+    end
+
   end
 end
