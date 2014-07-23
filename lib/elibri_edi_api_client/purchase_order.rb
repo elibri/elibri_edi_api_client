a5da06b9c84f4ccddf9f0cbc7a014ce753043ac2
Piotr Romańczuk <porbas@gmail.com>
2014-07-23 12:11:05 +0200

    api path for purchase_orders changed

--- lib/elibri_edi_api_client/purchase_order.rb
+++ lib/elibri_edi_api_client/purchase_order.rb
@@ -14,20 +14,20 @@ module ElibriEdiApiClient
     #dawać na sztywno w wywołaniach
     def self.create(data)
       o = new(data)
-      o.post 'v1/orders'
+      o.post 'v1/purchase_orders'
       o
     end
 
     def self.find(id_or_data)
       o = new(id_or_data)
-      o.get "v1/orders/:id"
+      o.get "v1/purchase_orders/:id"
       o
     end
 
     def create_message(data)
       klass = klass_from_kind(data[:kind])
       o = klass.new(data)
-      url = self.url_for 'v1/orders/:id/messages'
+      url = self.url_for 'v1/purchase_orders/:id/messages'
       o.post url
     end
 
