260449347e26525c62c80df0bd21e157d6840956
Piotr Romańczuk <porbas@gmail.com>
2014-12-05 12:58:48 +0100

    signed preorder form urls

--- lib/elibri_edi_api_client/preorder_form.rb
+++ lib/elibri_edi_api_client/preorder_form.rb
@@ -2,6 +2,10 @@
 module ElibriEdiApiClient
   class PreorderForm < Base
 
+    class << self
+      attr_accessor :config_secret
+    end
+
     def self.create(data)
       o = new(data)
       o.post 'v1/preorder_forms'
@@ -12,6 +16,18 @@ module ElibriEdiApiClient
       o = new(id_or_data)
       o.get "v1/preorder_forms/:id"
       o
+    end
+
+    def url_for_email(email)
+      signature = url_signature self.id, email
+      url_for "forms/preorder/:id/#{email}/#{signature}"
+    end
+
+    private
+    def url_signature(form_id, email)
+      @digest ||= OpenSSL::Digest::MD5.new
+      data = [form_id, email].join
+      OpenSSL::HMAC.hexdigest(@digest, ::ElibriEdiApiClient::PreorderForm.config_secret, data)
     end
   end
 end
